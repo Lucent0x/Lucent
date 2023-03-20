401bd011270bedc112413095e7078acd57246ec8
SHEDRACK OKPAKO <mail.lucidity@gmail.com>
2023-03-20 12:14:31 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const sender = document.getElementById("email");
 const message = document.getElementById("message");
 function sendEmail() {
   Email.send({
-    SecureToken : "ffb91c3d-9a52-4b35-b78d-753707581636",
+    SecureToken : "093f0024-15d7-433d-a57b-e1cc8e0a57e5",
     To : 'mail.lucidity@gmail.com',
     From : "mail.lucidity@gmail.com",
     Subject : `PORTFOLIO UPDATE!!! FROM ${sender.value}`,
